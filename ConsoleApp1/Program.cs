961c9649c4cb200739041bdd2b1964027d3a63f9
Elitkex <matematlag626@gmail.com>
2025-05-22 13:50:59 +0200

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("adj meg egy szamot 1-12 kozott: ");
+
+            int honap = int.Parse(Console.ReadLine());
+            if (honap < 1 || honap > 12)
+            {
+                Console.WriteLine("nem megfelelo ertek");
+            }
+
+            Console.ReadKey();
+        }
+        public string Evszak(int honap)
+        {
+            while(true)
+            {
+                if (honap == 3 || honap == 4 || honap == 5)
+                {
+                    Console.WriteLine("honap: tavasz");
+                }
+                if (honap == 6 || honap == 7 || honap == 8)
+                {
+                    Console.WriteLine("honap: nyár");
+                }
+                if (honap == 9 || honap == 10 || honap == 11)
+                {
+                    Console.WriteLine("honap: ősz");
+                }
+                if (honap == 1 || honap == 2 || honap == 12)
+                {
+                    Console.WriteLine("honap: tél");
+                }
+
+                else
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+            }
+
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using ConsoleApp1;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void InputGivesBackTel()
+        {
+            var valami = new Program();
+            string result = "tél";
+            Assert.AreEqual(result, 1);
+
+        }
+        public void InputGivesBackTavasz()
+        {
+            var valami = new Program();
+            string result = "tavasz";
+            Assert.AreEqual(result, 2);
+        }
+        public void InputGivesBackOsz()
+        {
+            var valami = new Program();
+            string result = "osz";
+            Assert.AreEqual(result, 3);
+        }
+        public void InputGivesBackNyar()
+        {
+            var valami = new Program();
+            string result = "Nyar";
+            Assert.AreEqual(result, 4);
+        }
+    }
+}
